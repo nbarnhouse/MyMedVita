bd58bd618ae4300abb192f93093bbd50b17806fe
Ryan Barfield <cadium828@gmail.com>
2024-04-23 10:02:04 -0500

    finalized the navbar, styled with material ui, fixed all links, added logo

--- src/components/AccessoryComponents/Nav/Nav.jsx
+++ src/components/AccessoryComponents/Nav/Nav.jsx
@@ -1,55 +1,65 @@
+// Import Modules
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Nav.css';
+import { useLocation, Link as RouterLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
+// Import Material UI
+import { AppBar, Toolbar, Button, Typography } from '@mui/material';
+
+// Import Custom CSS
+import './Nav.css';
+
+// Logo Image
+import logoImage from './images/logo_heart_only_MMV.png';
 
 function Nav() {
   const user = useSelector((store) => store.user);
+  const location = useLocation();
 
+  // Helper function to check if a link is active
+  const isActive = (pathname) => location.pathname === pathname;
+
+  // List of nav links to be mapped over and placed into navbar
+  const navButtonsList = [
+    'marketplace',
+    'features',
+    'about',
+    'contact',
+    user.id ? 'profile' : 'login',
+  ];
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">MyMedVita</h2>
-      </Link>
-      <div>
-        <Link className="navLink" to="/">
-          Home
-        </Link>
-
-        <Link className="navLink" to="/marketplace">
-          Marketplace
-        </Link>
-
-        <Link className="navLink" to="/features">
-          Features
-        </Link>
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-
-        <Link className="navLink" to="/contact">
-          Contact
-        </Link>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <div>
-            <Link className="navLink" to="/login">
-              Login
-            </Link>
-          </div>
-        )}
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/profile">
-              Profile
-            </Link>
-          </>
-        )}
-      </div>
-    </div>
+    <AppBar position="static" sx={{ backgroundColor: '#782cf6' }}>
+      <Toolbar>
+        {/* Logo */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button color="inherit" component={RouterLink} to="/">
+            <img
+              src={logoImage}
+              alt="a picture of a heart - the logo for the company"
+              style={{ maxWidth: 60, maxHeight: '100%' }}
+            />
+            MyMedVita
+          </Button>
+        </Typography>
+        {/* Rest of links from navButtonsList variable */}
+        {navButtonsList.map((path) => (
+          <Button
+            key={path}
+            color="inherit"
+            component={RouterLink}
+            to={`/${path}`}
+            sx={{
+              '&:hover': { backgroundColor: 'white', color: '#782cf6' },
+              backgroundColor: isActive(`/${path}`) ? 'white' : 'transparent',
+              color: isActive(`/${path}`) ? '#782cf6' : 'white',
+              margin: '0 4px',
+            }}
+          >
+            {path}
+          </Button>
+        ))}
+      </Toolbar>
+    </AppBar>
   );
 }
 
