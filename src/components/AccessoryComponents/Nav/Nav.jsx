bfa70fa9074fdec3bf6a855e2518ac7f87641478
Ryan Barfield <cadium828@gmail.com>
2024-04-22 20:53:25 -0500

    added initial links to each navbar component for testing - no styling applied

--- src/components/AccessoryComponents/Nav/Nav.jsx
+++ src/components/AccessoryComponents/Nav/Nav.jsx
@@ -13,32 +13,42 @@ function Nav() {
         <h2 className="nav-title">Prime Solo Project</h2>
       </Link>
       <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
+        <Link className="navLink" to="/">
+          Home
+        </Link>
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
+        <Link className="navLink" to="/marketplace">
+          Marketplace
+        </Link>
 
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
+        <Link className="navLink" to="/features">
+          Features
+        </Link>
 
         <Link className="navLink" to="/about">
           About
         </Link>
+
+        <Link className="navLink" to="/contact">
+          Contact
+        </Link>
+        {/* If no user is logged in, show these links */}
+        {!user.id && (
+          // If there's no user, show login/registration links
+          <div>
+            <Link className="navLink" to="/login">
+              Login
+            </Link>
+          </div>
+        )}
+        {/* If a user is logged in, show these links */}
+        {user.id && (
+          <>
+            <Link className="navLink" to="/profile">
+              Profile
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
