d3c0a717c6d16a5ceb44f3b07390c0f6b6f5fa12
Ryan Barfield <cadium828@gmail.com>
2024-04-22 21:01:30 -0500

    added navbar component to about page

--- src/components/UnprotectedComponents/AboutPage/AboutPage.jsx
+++ src/components/UnprotectedComponents/AboutPage/AboutPage.jsx
@@ -1,9 +1,13 @@
 // Import 3rd Party Libraries
 import React from 'react';
 
+// Import Custom Components
+import NavBar from '../../AccessoryComponents/Nav/Nav';
+
 function AboutPage() {
   return (
     <div>
+      <NavBar />
       <h1>About Page</h1>
     </div>
   );
