6d13faa410bdae525e7d7b0d5c0bba603e1682dd
Ryan Barfield <cadium828@gmail.com>
2024-04-22 21:00:41 -0500

    added navbar component to features page

--- src/components/UnprotectedComponents/FeaturesPage/FeaturesPage.jsx
+++ src/components/UnprotectedComponents/FeaturesPage/FeaturesPage.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 // CUSTOM COMPONENTS
+import NavBar from '../../AccessoryComponents/Nav/Nav';
 
 function FeaturesPage() {
   const history = useHistory();
 
   return (
     <>
+      <NavBar />
       <h1>Features Page</h1>
     </>
   );
