07e514e1514b26ac3c56931f75866fe43a05de19
Ryan Barfield <cadium828@gmail.com>
2024-04-24 18:01:47 -0500

    created the contact page css, completed imports, added functions to handle form submission and user input changes

--- src/components/UnprotectedComponents/ContactPage/ContactPage.jsx
+++ src/components/UnprotectedComponents/ContactPage/ContactPage.jsx
@@ -1,13 +1,45 @@
 // Import 3rd Party Libraries
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
 // CUSTOM COMPONENTS
 import NavBar from '../../AccessoryComponents/Nav/Nav';
-import Nav from '../../AccessoryComponents/Nav/Nav';
+
+// Import Material UI and Custom CSS
+import { TextField, Button, Grid, Typography, Snackbar } from '@mui/material';
+import './ContactPage.css';
 
 function ContactPage() {
-  const history = useHistory();
+  cont[(newContact, setNewContact)] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  // Handle user input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewContact({ ...newContact, [name]: value });
+  };
+
+  // Handle Form Submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSnackbarOpen(true);
+    setNewContact({
+      name: '',
+      email: '',
+      message: '',
+    });
+  };
+
+  // Function to close Snackbar
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false);
+  };
 
   return (
     <>
