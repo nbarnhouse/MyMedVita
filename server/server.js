60a960bd7fa933c5a53fae09da285bda4befb853
Alvin Graham <grah0071@gmail.com>
2024-04-23 20:55:09 -0500

    Register category router in server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route Includes
 const userRouter = require('./routes/user.router');
 const geoRouter = require('./routes/geocode.router');
+const categoryRouter = require('./routes/category.router');
 
 // Express Middleware
 app.use(express.json());
@@ -26,6 +27,7 @@ app.use(passport.session());
 // Routes
 app.use('/api/user', userRouter);
 app.use('/api/location', geoRouter);
+app.use('/api/category', categoryRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
