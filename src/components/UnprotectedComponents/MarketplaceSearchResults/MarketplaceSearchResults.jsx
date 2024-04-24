350202a669f7f41471135ab3a4bbec36ce9c2deb
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2024-04-24 09:21:38 -0500

    search distance is adjustable based on user input

--- src/components/UnprotectedComponents/MarketplaceSearchResults/MarketplaceSearchResults.jsx
+++ src/components/UnprotectedComponents/MarketplaceSearchResults/MarketplaceSearchResults.jsx
@@ -5,10 +5,16 @@ import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './MarketplaceSearchResults.css';
 
+// CUSTOM COMPONENTS
 import NavBar from '../../AccessoryComponents/Nav/Nav';
 
 export default function MarketplaceSearchResults() {
   const history = useHistory();
+  const [zip, setZip] = useState('');
+  const [distance, setDistance] = useState('');
+  const [filteredPoints, setFilteredPoints] = useState([]);
+  const [originalZip, setOriginalZip] = useState('');
+  const [originalDistance, setOriginalDistance] = useState('');
 
   const centerLat = 36.1539;
   const centerLon = -95.9927;
@@ -19,7 +25,38 @@ export default function MarketplaceSearchResults() {
     [35.9334, -95.8776],
   ];
 
-  const maxDistance = 150; // Maximum distance in miles
+  const handleZipSearch = (event) => {
+    event.preventDefault();
+    if (zip.length !== 5 || isNaN(zip)) {
+      alert('Please enter a valid 5-digit zip code.');
+
+      setZip('');
+      setDistance('');
+      return;
+    }
+
+    console.log(`Zip Code: ${zip} and Distance:${distance}`);
+    const maxDistance = parseFloat(distance); // Maximum distance in miles
+    const filteredPoints = filterPoints(
+      centerLat,
+      centerLon,
+      points,
+      maxDistance
+    );
+    setFilteredPoints(filteredPoints);
+
+    setOriginalZip(zip);
+    setOriginalDistance(distance);
+
+    setZip('');
+    setDistance('');
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleZipSearch(event);
+    }
+  };
 
   function haversine(lat1, lon1, lat2, lon2) {
     // Convert latitude and longitude from degrees to radians
@@ -45,17 +82,6 @@ export default function MarketplaceSearchResults() {
     return filteredPoints;
   }
 
-  const filteredPoints = filterPoints(
-    centerLat,
-    centerLon,
-    points,
-    maxDistance
-  );
-
-  const distances = filteredPoints.map((point) =>
-    haversine(centerLat, centerLon, point[0], point[1])
-  );
-
   const handleBack = () => {
     history.push('/search');
   };
@@ -64,13 +90,44 @@ export default function MarketplaceSearchResults() {
     <>
       <NavBar />
       <div className="container">
+        <div className="input container">
+          <form onSubmit={handleZipSearch}>
+            <label htmlFor="zipSearch">Zip Code:</label>
+            <input
+              type="text"
+              id="zipSearch"
+              name="zipSearch"
+              placeholder="Zip Code"
+              value={zip}
+              onChange={(event) => setZip(event.target.value)}
+              onKeyDown={handleKeyDown}
+            ></input>
+
+            <label htmlFor="distanceSearch">Distance(Miles):</label>
+            <input
+              type="number"
+              id="distanceSearch"
+              name="distanceSearch"
+              placeholder="Distance(Miles)"
+              value={distance}
+              onChange={(event) => setDistance(event.target.value)}
+            ></input>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
         <div className="result-container">
           <h1>MyMedVita Search Results</h1>
-          <h4>Search Parameters: MRI - , 74011, No limit on Miles</h4>
+          <h4>
+            Search Parameters: MRI - , Zip: {originalZip}, {originalDistance}{' '}
+            Miles
+          </h4>
 
           <p>
-            NOTE: setting zip as center on map. Test markers are: [36.1539,
-            -95.9927] and [33.6609, -95.5555]
+            NOTE: setting zip 74011 as center on map. (This is hardcoded for
+            now). Test markers are: [36.1539, -95.9927] and [33.6609, -95.5555]
+            within 30 miles. Test marker [33.6609, -95.5555 ] is within 200
+            miles.
           </p>
           <h2>Filtered Data</h2>
           <table className="centered-table">
@@ -84,29 +141,18 @@ export default function MarketplaceSearchResults() {
               {filteredPoints.map((point, index) => (
                 <tr key={index}>
                   <td>{point.join(', ')}</td>
-                  <td>{distances[index].toFixed(0)}</td>
+                  <td>
+                    {haversine(
+                      centerLat,
+                      centerLon,
+                      point[0],
+                      point[1]
+                    ).toFixed(0)}
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
-
-          <div className="unfiltered-data">
-            <h2>All Data</h2>
-            <table className="centered-table">
-              <thead>
-                <tr>
-                  <th>Coordinate</th>
-                </tr>
-              </thead>
-              <tbody>
-                {points.map((point, index) => (
-                  <tr key={index}>
-                    <td>{point.join(', ')}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
 
           <button onClick={handleBack}>Back</button>
         </div>
