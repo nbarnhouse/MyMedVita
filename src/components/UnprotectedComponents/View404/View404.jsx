a36924f0b09c7d55610d6bf86d1af5bedd9a3afc
Ryan Barfield <cadium828@gmail.com>
2024-04-25 12:03:37 -0500

    added 404 error verbiage and styling for View404 component

--- src/components/UnprotectedComponents/View404/View404.jsx
+++ src/components/UnprotectedComponents/View404/View404.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 
 // CUSTOM COMPONENTS
 import NavBar from '../../AccessoryComponents/Nav/Nav';
+import notFoundImage from './images/not_found.avif';
+
+// Import Custom CSS
+import './View404.css';
 
 function View404() {
   return (
     <>
       <NavBar />
-      <p>404 Page</p>
+      <div className="error-container">
+        <p className="error-number">ERROR: 404</p>
+        <h1 className="error-header">
+          Uh Oh The Page You're Looking For Either Can't Be Found Or You Don't
+          Have Access To It!
+        </h1>
+      </div>
     </>
   );
 }
