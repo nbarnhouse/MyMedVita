f8b945c707cdcb98969cb628113aab233bcd6c3e
Ryan Barfield <cadium828@gmail.com>
2024-04-25 09:45:04 -0500

    merged changes and fix the issues with the conflicts

--- src/components/UnprotectedComponents/MarketplacePage/MarketplacePage.jsx
+++ src/components/UnprotectedComponents/MarketplacePage/MarketplacePage.jsx
@@ -3,32 +3,49 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import './MarketplacePage.css';
-
 // CUSTOM COMPONENTS
 import NavBar from '../../AccessoryComponents/Nav/Nav';
 import ProcedureSearchBar from './ProcedureSearchBar/ProcedureSearchBar';
 
+// Import Material UI and Custom CSS
+import {
+  Typography,
+  Grid,
+  TextField,
+  Select,
+  MenuItem,
+  InputAdornment,
+  Button,
+} from '@mui/material';
+import MapIcon from '@mui/icons-material/Map';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import './MarketplacePage.css';
+
 function MarketplacePage() {
+  const [query, setQuery] = useState({
+    searchTerm: '',
+    zip: '',
+    distance: 25,
+  });
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const [zip, setZip] = useState('');
-  const [searchDistance, setSearchDistance] = useState('');
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      handleZipSearch(event);
-    }
+  // Function to handle user input change
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setQuery((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleZipSearch = (event) => {
+  // Function to handle user form submission
+  const handleSubmit = (event) => {
+    // Prevent the page from refreshing
     event.preventDefault();
-    if (zip.length !== 5 || isNaN(zip)) {
-      alert('Please enter a valid 5-digit zip code.');
 
-      setZip('');
-      setSearchDistance('');
+    // Validate Zip Code = 5 digits and is a number
+    const { zip, distance } = query;
+    if (zip.length !== 5 || isNaN(Number(zip))) {
+      alert('Please enter a valid 5-digit zip code.');
+      setQuery((prev) => ({ ...prev, zip: '' }));
       return;
     }
 
@@ -36,46 +53,65 @@ function MarketplacePage() {
       type: 'SUBMIT_DISTANCE_DATA',
       payload: {
         zip,
-        searchDistance,
+        distance,
       },
     });
-
-    console.log(`Zip Code: ${zip} and searchDistance:${searchDistance}`);
     history.push('/results');
   };
 
   return (
     <>
       <NavBar />
-      <h1>MyMedVita Marketplace</h1>
-      <div className="container">
-        <div className="input container">
-          <form onSubmit={handleZipSearch}>
-            <ProcedureSearchBar />
-
-            <label htmlFor="zipSearch">Zip Code:</label>
-            <input
-              type="text"
-              id="zipSearch"
-              name="zipSearch"
-              placeholder="Zip Code"
-              value={zip}
-              onChange={(event) => setZip(event.target.value)}
-            ></input>
-
-            <label htmlFor="searchDistanceSearch">Distance (Miles):</label>
-            <input
-              type="number"
-              id="searchDistanceSearch"
-              name="searchDistanceSearch"
-              placeholder="Distance (Miles)"
-              value={searchDistance}
-              onChange={(event) => setSearchDistance(event.target.value)}
-              onKeyDown={handleKeyDown}
-            ></input>
-            <button type="submit">Shop</button>
-          </form>
-        </div>
+      <div className="search-container">
+        <h1>MyMedVita Marketplace</h1>
+        <p>Click HERE for a CPT Reference Guide</p>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2} wrap="wrap">
+            <Grid item xs={12}>
+              <ProcedureSearchBar />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Zip Code"
+                name="zip"
+                variant="outlined"
+                onChange={handleChange}
+                value={query.zip}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LocationOnIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                select
+                label="Distance (Miles)"
+                name="distance"
+                variant="outlined"
+                value={query.distance}
+                onChange={handleChange}
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MapIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              >
+                <MenuItem value="5">5</MenuItem>
+                <MenuItem value="10">10</MenuItem>
+                <MenuItem value="25">25</MenuItem>
+                <MenuItem value="50">50</MenuItem>
+              </TextField>
+            </Grid>
+          </Grid>
+        </form>
       </div>
     </>
   );
