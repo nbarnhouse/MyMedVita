9cc23658d65ebb002f1b3405e13c9023e1a7e62a
Alvin Graham <grah0071@gmail.com>
2024-04-24 20:37:23 -0500

    Validate env file variablse read in route

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -14,7 +14,27 @@ const transporter = nodemailer.createTransport({
     pass: process.env.CONTACT_PASSWORD,
   },
 });
+
 // email message to 'support@myMedvita.com'
 // POST route emails support and returns status to client
+router.post('/', (req, res) => {
+  const messageOptions = {
+    from: req.body.email,
+    to: process.env.CONTACT_EMAIL,
+    subject: `MyMedVita User Question/Feedback - ${req.body.name}`,
+    text: req.body.message,
+  };
+
+  try {
+    console.log('Transporter - Service:', process.env.CONTACT_SERVICE);
+    console.log('Transporter - user:', process.env.CONTACT_USER);
+    console.log('Transporter - Service:', process.env.CONTACT_PASSWORD);
+    console.log('Transporter:', transporter.options);
+  } catch (err) {
+    console.error('Send');
+  }
+
+  res.sendStatus(200);
+});
 
 module.exports = router;
