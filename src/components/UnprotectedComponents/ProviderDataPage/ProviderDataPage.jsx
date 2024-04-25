f16cb1dfcda63d59eabbaf25fc1d5c870a65c479
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2024-04-24 21:57:44 -0500

    updated App.jsx route

--- src/components/UnprotectedComponents/ProviderDataPage/ProviderDataPage.jsx
+++ src/components/UnprotectedComponents/ProviderDataPage/ProviderDataPage.jsx
@@ -3,13 +3,29 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 // CUSTOM COMPONENTS
+import NavBar from '../../AccessoryComponents/Nav/Nav';
 
 function ProviderDataPage() {
   const history = useHistory();
 
+  const handleBack = () => {
+    history.push('/results');
+  };
+
   return (
     <>
-      <h1>MP SEARCH RESULTS Page</h1>
+      <NavBar />
+      <h1>Provider Details</h1>
+      <div className="left-contianer">
+        <h4>Provider:</h4>
+        <h4>Procedure:</h4>
+        <h4>Address:</h4>
+        <h4>Pricing:</h4>
+        <h4>Phone Number:</h4>
+        <h4>Distance:</h4>
+      </div>
+
+      <button onClick={handleBack}>Back</button>
     </>
   );
 }
