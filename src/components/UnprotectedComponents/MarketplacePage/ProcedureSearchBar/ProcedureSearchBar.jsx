8e7747c2828fcf6621c0d8ec9ed16fb19267dd30
Ryan Barfield <cadium828@gmail.com>
2024-04-25 09:57:25 -0500

    updated imports

--- src/components/UnprotectedComponents/MarketplacePage/ProcedureSearchBar/ProcedureSearchBar.jsx
+++ src/components/UnprotectedComponents/MarketplacePage/ProcedureSearchBar/ProcedureSearchBar.jsx
@@ -1,56 +1,65 @@
+// Import 3rd Party Libraries
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import axios from 'axios';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+
+// Import Material UI
+import { InputAdornment, TextField, Autocomplete } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 function ProcedureSearchBar() {
-    const history = useHistory();
-    const [searchQuery, setSearchQuery] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
-    const [open, setOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [open, setOpen] = useState(false);
 
-    const handleInputChange = async (event) => {
-        const query = event.target.value;
-        setSearchQuery(query);
+  const handleInputChange = async (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
 
-        try {
-            // Make a request to your backend API to fetch suggestions
-            const response = await axios.get(`/api/search/${query}`)
-            const data = await response.data;
-            setSuggestions(data);
-            setOpen(true); // Open the dropdown when there are suggestions
-        } catch (error) {
-            console.error('Error fetching suggestions:', error);
+    try {
+      // Make a request to your backend API to fetch suggestions
+      const response = await axios.get(`/api/search/${query}`);
+      const data = await response.data;
+      setSuggestions(data);
+      setOpen(true); // Open the dropdown when there are suggestions
+    } catch (error) {
+      console.error('Error fetching suggestions:', error);
+    }
+
+    if (query === '') {
+      setOpen(false); // Close the dropdown when query is empty
+    }
+  };
+
+  return (
+    <>
+      <Autocomplete
+        freeSolo
+        options={suggestions}
+        getOptionLabel={(option) =>
+          `${option.primary_code} - ${option.description}`
         }
-        
-        if (query === '') {
-            setOpen(false); // Close the dropdown when query is empty
-        }
-    };
-
-    return (
-        <>
-            <p>Search:</p>
-            <Autocomplete
-                freeSolo
-                options={suggestions}
-                getOptionLabel={(option) => `${option.primary_code} - ${option.description}`}
-                open={open}
-                onInputChange={handleInputChange}
-                renderInput={(params) => (
-                    <TextField
-                        {...params}
-                        label="Search"
-                        margin="normal"
-                        variant="outlined"
-                        value={searchQuery}
-                        onChange={handleInputChange}
-                    />
-                )}
-            />
-        </>
-    );
+        open={open}
+        onInputChange={handleInputChange}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search"
+            margin="normal"
+            variant="outlined"
+            value={searchQuery}
+            onChange={handleInputChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        )}
+      />
+    </>
+  );
 }
 
 export default ProcedureSearchBar;
