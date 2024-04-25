6afdbd3cb7d5abd3f6bad6eddc75f72271ae0eba
Alvin Graham <grah0071@gmail.com>
2024-04-24 19:30:08 -0500

    Establish email Transporter and .ENV variables

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const pool = require('../modules/pool');
+const nodemailer = require('nodemailer');
 const router = express.Router();
 require('dotenv').config();
 
+// Define CONTACT_SERVICE, CONTACT_USER, CONTACT_PASSWORD, and
+// CONTACT_EMAIL in environment variable
+
+// Establish email transporter
+const transporter = nodemailer.createTransport({
+  service: process.env.CONTACT_SERVICE,
+  auth: {
+    user: process.env.CONTACT_USER,
+    pass: process.env.CONTACT_PASSWORD,
+  },
+});
 // email message to 'support@myMedvita.com'
 // POST route emails support and returns status to client
 
