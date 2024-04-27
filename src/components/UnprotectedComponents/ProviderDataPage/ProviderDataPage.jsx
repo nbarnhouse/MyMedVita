d598e321ef42cf96f967929c8fafafb3390109b0
Ryan Barfield <cadium828@gmail.com>
2024-04-27 12:49:09 -0500

    added styling, structure, reformatted to map the elements to not repeat myself, added box shadows

--- src/components/UnprotectedComponents/ProviderDataPage/ProviderDataPage.jsx
+++ src/components/UnprotectedComponents/ProviderDataPage/ProviderDataPage.jsx
@@ -1,6 +1,6 @@
 // Import 3rd Party Libraries
-import { useSelector } from 'react-redux';
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
@@ -14,19 +14,18 @@ import './ProviderDataPage.css';
 function ProviderDataPage() {
   const history = useHistory();
   const location = useLocation();
-  const { provider } = location.state || {};
+  const provider = location.state?.provider || {};
 
+  useEffect(() => {
+    console.log('Provider on details page:', provider);
+  }, [provider]);
+
+  // Function to user back click
   const handleBackClick = () => {
     history.push('/results');
   };
 
-  useEffect(() => {
-    console.log('PROVIDER ON DETAILS PAGE:', provider);
-  }, []);
-
-  // Check if provider object contains valid latitude and longitude values
-  const isValidProvider =
-    provider && provider.provider_lat && provider.provider_long;
+  const isValidProvider = provider.provider_lat && provider.provider_long;
   const centerLat = isValidProvider ? parseFloat(provider.provider_lat) : 0;
   const centerLon = isValidProvider ? parseFloat(provider.provider_long) : 0;
 
@@ -39,122 +38,67 @@ function ProviderDataPage() {
         </div>
         <div className="provider-details-bottom-container">
           <div className="provider-details-information-container">
-            <div className="provider-info-div">
-              <h5>Provider</h5>
-              <p className="provider-info-paragraph">
-                {provider.provider_first_name && provider.provider_first_name}{' '}
-                {provider.provider_last_name && provider.provider_last_name}{' '}
-                {provider.provider_credential && provider.provider_credential}
-              </p>
-            </div>
-
-            <div className="provider-info-div">
-              <h5>Procedure</h5>
-              <p className="provider-info-paragraph">
-                {provider.CPT_CODE && provider.CPT_CODE}
-              </p>
-            </div>
-
-            <div className="provider-info-div">
-              <h5>Address</h5>
-              <p className="provider-info-paragraph"></p>
-            </div>
-
-            <div className="provider-info-div">
-              <h5>Pricing</h5>
-              <p className="provider-info-paragraph">
-                $
-                {provider.negotiated_rate &&
-                  Math.floor(provider.negotiated_rate)}
-              </p>
-            </div>
-
-            <div className="provider-info-div">
-              <h5>Phone Number</h5>
-              <p className="provider-info-paragraph"></p>
-            </div>
-
-            <div className="provider-info-div">
-              <h5>Distance</h5>
-              <p className="provider-info-paragraph"></p>
-            </div>
-
-            <ul>
-              <li>
-                <strong>CMS Specialty Name:</strong>{' '}
-                {provider.CMS_Specialty_Name}
-              </li>
-              <li>
-                <strong>CPT Code:</strong> {provider.CPT_CODE}
-              </li>
-              <li>
-                <strong>ID:</strong> {provider.id}
-              </li>
-              <li>
-                <strong>Insurer ID:</strong> {provider.insurer_id}
-              </li>
-              <li>
-                <strong>Negotiated Rate:</strong> {provider.negotiated_rate}
-              </li>
-              <li>
-                <strong>Provider Address:</strong> {provider.provider_address}
-              </li>
-              <li>
-                <strong>Provider City:</strong> {provider.provider_city}
-              </li>
-              <li>
-                <strong>Provider Credential:</strong>{' '}
-                {provider.provider_credential}
-              </li>
-              <li>
-                <strong>Provider First Name:</strong>{' '}
-                {provider.provider_first_name}
-              </li>
-              <li>
-                <strong>Provider Last Name:</strong>{' '}
-                {provider.provider_last_name}
-              </li>
-              <li>
-                <strong>Provider Latitude:</strong> {provider.provider_lat}
-              </li>
-              <li>
-                <strong>Provider Longitude:</strong> {provider.provider_long}
-              </li>
-              <li>
-                <strong>Provider Organization Name:</strong>{' '}
-                {provider.provider_organization_name}
-              </li>
-              <li>
-                <strong>Provider Phone:</strong> {provider.provider_phone}
-              </li>
-              <li>
-                <strong>Provider State:</strong> {provider.provider_state}
-              </li>
-              <li>
-                <strong>Provider Zip:</strong> {provider.provider_zip}
-              </li>
-            </ul>
+            {[
+              {
+                label: 'Provider',
+                content: `${provider.provider_first_name || ''} ${
+                  provider.provider_last_name || ''
+                } ${provider.provider_credential || ''}`,
+              },
+              {
+                label: 'Procedure',
+                content: provider.CPT_CODE || 'No CPT Code Listed',
+              },
+              {
+                label: 'Address',
+                content: (
+                  <>
+                    {provider.provider_address || ''}
+                    <br />
+                    {`${provider.provider_city || ''} ${
+                      provider.provider_state || ''
+                    } ${provider.provider_zip || ''}`}
+                  </>
+                ),
+              },
+              {
+                label: 'Pricing',
+                content: `$${
+                  provider.negotiated_rate
+                    ? Math.floor(provider.negotiated_rate)
+                    : 'No Price Listed'
+                }`,
+              },
+              {
+                label: 'Phone Number',
+                content: provider.provider_phone || 'No Number Listed',
+              },
+              { label: 'Distance', content: 'Need to Add Distance Here' },
+            ].map((info, index) => (
+              <div key={index} className="provider-info-div">
+                <h5 className="provider-info-header">{info.label}</h5>
+                <p className="provider-info-paragraph">{info.content}</p>
+              </div>
+            ))}
           </div>
           <div className="provider-details-map-container">
             {isValidProvider ? (
-              <div className="map-container">
-                <MapContainer
-                  className="map"
-                  center={[centerLat, centerLon]}
-                  zoom={12}
-                >
-                  <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  />
-                  <Marker position={[centerLat, centerLon]}>
-                    <Popup>
-                      {provider.provider_last_name},{' '}
-                      {provider.provider_first_name}
-                    </Popup>
-                  </Marker>
-                </MapContainer>
-              </div>
+              <MapContainer
+                className="map"
+                center={[centerLat, centerLon]}
+                zoom={12}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[centerLat, centerLon]}>
+                  <Popup>
+                    {provider.provider_last_name},{' '}
+                    {provider.provider_first_name}
+                  </Popup>
+                </Marker>
+              </MapContainer>
             ) : (
               <p>No location data available for this provider.</p>
             )}
@@ -164,14 +108,8 @@ function ProviderDataPage() {
           <Button
             variant="contained"
             size="large"
-            style={{
-              backgroundColor: '#782cf6',
-              color: 'white',
-            }}
-            sx={{
-              transition: 'transform 0.3s',
-              '&:hover': { transform: 'scale(1.1)' },
-            }}
+            style={{ backgroundColor: '#782cf6', color: 'white' }}
+            onClick={handleBackClick}
           >
             Back
           </Button>
