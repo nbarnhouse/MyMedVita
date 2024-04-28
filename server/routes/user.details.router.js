0d55fbdb1e3035a3a4919e3455a7afe0c6e7272c
jessiyang1996 <jessiyang1996@gmail.com>
2024-04-28 18:40:03 -0500

    user details get and put routs completed

--- server/routes/user.details.router.js
+++ server/routes/user.details.router.js
@@ -20,12 +20,36 @@ router.get('/', (req, res) => {
 });
 
 // PUT (UPDATE) Route
-router.post('/', (req, res) => {
-  const sqlText = `
+router.put('/:id', (req, res) => {
+  const queryText = `
     UPDATE "user"
     SET "first_name" = $1, "last_name" = $2, "email" = $3, "phone" = $4, "dob" = $5, "gender" = $6, "street_address" = $7, "city" = $8, "state" = $9, "zip" = $10
     WHERE "id" = $11;
   `;
+
+  sqlValues = [
+    req.body.first_name,
+    req.body.last_name,
+    req.body.email,
+    req.body.phone,
+    req.body.dob,
+    req.body.gender,
+    req.body.street_address,
+    req.body.city,
+    req.body.state,
+    req.body.zip,
+    req.body.id,
+  ];
+
+  pool
+    .query(queryText, sqlValues)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((dbError) => {
+      console.log('Error in updating user details', dbError);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
