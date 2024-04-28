97bfcfbc96050bb06fdcce7a45b3948e7cb1e4a9
jessiyang1996 <jessiyang1996@gmail.com>
2024-04-28 15:43:20 -0500

    added get route

--- server/routes/user.details.router.js
+++ server/routes/user.details.router.js
@@ -2,7 +2,22 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-// GET (READ) Route
+// GET (READ) user details
+router.get('/', (req, res) => {
+  const sqlText = `
+  SELECT "id", "first_name", "last_name", "email", "phone", "dob", "gender", "street_address", "city", "state", "zip" FROM "user" WHERE "id"=1;
+  `;
+
+  pool
+    .query(sqlText)
+    .then((dbRes) => {
+      res.send(dbRes.rows);
+    })
+    .catch((dbError) => {
+      console.log('Error in retrieving user details from DB', dbError);
+      res.sendStatus(500);
+    });
+});
 
 // PUT (UPDATE) Route
 
