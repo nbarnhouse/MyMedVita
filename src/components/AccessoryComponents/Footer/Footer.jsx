c017ec5a3acc1d712e22d7c18a5d3359f71c6c3a
Ryan Barfield <cadium828@gmail.com>
2024-04-28 14:57:49 -0500

    changed div layout of footer component

--- src/components/AccessoryComponents/Footer/Footer.jsx
+++ src/components/AccessoryComponents/Footer/Footer.jsx
@@ -1,13 +1,13 @@
+// Import 3rd Party Libraries
 import React from 'react';
 import './Footer.css';
 
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return (
+    <footer>
+      <h5>&copy; Prime Digital Academy</h5>
+    </footer>
+  );
 }
 
 export default Footer;
