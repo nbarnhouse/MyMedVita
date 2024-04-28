261d141d98881605c35deea5cf22315bcb7aa951
sseang <142060734+sseang@users.noreply.github.com>
2024-04-28 13:05:40 -0500

    created UserNavBar file

---
+++ src/components/ProtectedComponents/UserNavBar/UserNavBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useLocation, Link as RouterLink } from 'react-router-dom';
+
+function UserNavBar() {
+  const location = useLocation();
+}
+
+export default UserNavBar;
