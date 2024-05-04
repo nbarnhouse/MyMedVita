d9387196e4499e3c527a87de540252206908eef9
sseang <142060734+sseang@users.noreply.github.com>
2024-05-04 11:47:09 -0500

    align text in logout button

--- src/components/AccessoryComponents/LogOutButton/LogOutButton.jsx
+++ src/components/AccessoryComponents/LogOutButton/LogOutButton.jsx
@@ -3,12 +3,21 @@ import { useDispatch } from 'react-redux';
 import LogoutIcon from '@mui/icons-material/Logout';
 import Button from '@mui/material/Button';
 import './LogOutButton.css';
+import { Typography } from '@mui/material';
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
   return (
-    <Button className="Logout" onClick={() => dispatch({ type: 'LOGOUT' })}>
+    <Button
+      className="Logout"
+      style={{
+        textTransform: 'none',
+        color: 'black',
+        fontSize: '16px',
+      }}
+      onClick={() => dispatch({ type: 'LOGOUT' })}>
       <LogoutIcon fontSize="small" />
+      Log Out
     </Button>
   );
 }
