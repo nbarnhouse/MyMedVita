61f0c45138d10f6215c77b6104882708fb92e3a8
jessiyang1996 <jessiyang1996@gmail.com>
2024-05-04 22:44:48 -0500

    formatted the DOB so that it is more user-friendly

--- src/components/ProtectedComponents/UserPage/UserPage.jsx
+++ src/components/ProtectedComponents/UserPage/UserPage.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import moment from 'moment';
 
 // CUSTOM COMPONENTS
 import Navbar from '../../AccessoryComponents/Nav/Nav';
@@ -13,6 +14,8 @@ function UserPage() {
   const dispatch = useDispatch();
   const history = useHistory();
   const userDetails = useSelector((store) => store.userDetails);
+
+  const formattedDate = moment(userDetails.dob).format('MM-DD-YYYY');
 
   useEffect(() => {
     dispatch({
@@ -37,7 +40,7 @@ function UserPage() {
           <p>Last Name: {userDetails.last_name}</p>
           <p>Email Address: {userDetails.email}</p>
           <p>Phone Number: {userDetails.phone}</p>
-          <p>DOB: {userDetails.dob}</p>
+          <p>DOB: {formattedDate}</p>
           <p>Gender: {userDetails.gender}</p>
           <h4>Address:</h4>
           <p>Street: {userDetails.street_address}</p>{' '}
