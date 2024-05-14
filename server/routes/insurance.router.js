d3cbccf7d0313dbd637e2bdbdb8266c5313281b3
Alvin Graham <grah0071@gmail.com>
2024-05-13 19:08:27 -0500

    Add API Doc to insurance.router.js

--- server/routes/insurance.router.js
+++ server/routes/insurance.router.js
@@ -2,6 +2,17 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+/**
+ * @api {get} /api/insurance/ Get list of insurance providers
+ * @apiName GetInsurerList
+ * @apiGroup Insurance
+ *
+ * @apiSuccess {Object[]} insurers  Array of Insurance Providers.
+ * @apiSuccess {Number} insurer.id id of Insurance Provider.
+ * @apiSuccess {String} insurer.insurer_name Name of Insurance Provider.
+ * @apiSuccess {Number} insurer.insurer_code Unique (power of 2) code of Insurance Provider.
+ */
+
 // GET Insurance list from database
 router.get('/', (req, res) => {
   const queryText = `SELECT * FROM "insurance_providers" ORDER BY "insurer_code";`;
